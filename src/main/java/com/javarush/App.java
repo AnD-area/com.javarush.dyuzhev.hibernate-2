b9de75f5c33b62dd4bda8316f68397361069549f
Anton <mtvv2005@yandex.by>
2024-08-09 10:38:48 +0300

    Add Data Access Objects classes

--- src/main/java/com/javarush/App.java
+++ src/main/java/com/javarush/App.java
@@ -1,15 +1,31 @@
 package com.javarush;
 
+import com.javarush.dao.*;
 import com.javarush.entity.*;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-/**
- * Hello world!
- */
+
 public class App {
 
     public final SessionFactory sessionFactory;
+
+    private final ActorDAO actorDAO;
+    private final AddressDAO addressDAO;
+    private final CategoryDAO categoryDAO;
+    private final CityDAO cityDAO;
+    private final CountryDAO countryDAO;
+    private final CustomerDAO customerDAO;
+    private final FeatureDAO featureDAO;
+    private final FilmDAO filmDAO;
+    private final FilmTextDAO filmTextDAO;
+    private final InventoryDAO inventoryDAO;
+    private final LanguageDAO languageDAO;
+    private final PaymentDAO paymentDAO;
+    private final RatingDAO ratingDAO;
+    private final RentalDAO rentalDAO;
+    private final StaffDAO staffDAO;
+    private final StoreDAO storeDAO;
 
     public App() {
         sessionFactory = new Configuration()
@@ -30,10 +46,32 @@ public class App {
                 .addAnnotatedClass(Staff.class)
                 .addAnnotatedClass(Store.class)
                 .buildSessionFactory();
+
+        actorDAO = new ActorDAO(sessionFactory);
+        addressDAO = new AddressDAO(sessionFactory);
+        categoryDAO = new CategoryDAO(sessionFactory);
+        cityDAO = new CityDAO(sessionFactory);
+        countryDAO = new CountryDAO(sessionFactory);
+        customerDAO = new CustomerDAO(sessionFactory);
+        featureDAO = new FeatureDAO(sessionFactory);
+        filmDAO = new FilmDAO(sessionFactory);
+        filmTextDAO = new FilmTextDAO(sessionFactory);
+        inventoryDAO = new InventoryDAO(sessionFactory);
+        languageDAO = new LanguageDAO(sessionFactory);
+        paymentDAO = new PaymentDAO(sessionFactory);
+        ratingDAO = new RatingDAO(sessionFactory);
+        rentalDAO = new RentalDAO(sessionFactory);
+        staffDAO = new StaffDAO(sessionFactory);
+        storeDAO = new StoreDAO(sessionFactory);
     }
 
     public static void main(String[] args) {
 
         App app = new App();
+        Customer customer = app.createCustomer();
+    }
+
+    private Customer createCustomer() {
+        return null;
     }
 }
